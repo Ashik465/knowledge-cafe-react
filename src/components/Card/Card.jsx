bdeeca0673e7797743dfbaf760238a05f6db6639
Ashik465 <helloashikul@gmail.com>
2023-03-31 04:16:37 +0600

    blog-card-load-data- done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Card = (props) => {
     const {blogTitle,authorName,readTime,publishDate,authorImage,blogCoverImg} =props.blog
@@ -7,12 +9,27 @@ const Card = (props) => {
             <div className="card card-compact w-full h-[100%] bg-base-100 shadow-xl  ">
   <figure ><img className=' rounded-lg ' src={blogCoverImg} alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-
+    <div className='flex justify-between flex-row' >
+        <div className='flex gap-5'>
+       
+          <img className="w-14 rounded-full"src={authorImage} />
+          <div className='flex flex-col '>
+             <h1 className=' font-bold text-2xl'>{authorName}</h1>
+             <p className=' text-[#11111199]'>{publishDate} </p>
+         
+              </div>
+        </div>
+        <div>
+         
+        <p className='text-[#11111199] text-xl '>{readTime} min read <FontAwesomeIcon className='cursor-pointer' icon={faBookmark} /> </p>
+        </div>
     </div>
+    <h1 className='py-5 font-bold text-4xl'>{blogTitle}</h1>
+
+     <p className='text-xl pb-4'>#beginners  #programming</p>
+
+     <p className='text-[#6047EC] text-xl underline pb-4 cursor-pointer'>Mark as read</p>
+    
     <div className='mb-5'><hr /></div>
 
   </div>
